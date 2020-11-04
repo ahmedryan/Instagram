7097d2617574f6e0b0eb20af06181ab15663231c
el_revelador <bsse1011@iit.du.ac.bd>
2020-11-04 18:01:59 +0600

    55 mins done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,37 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-2 offset-1 pl-5 pr-5">
+            <img src="https://rb.gy/nghnjy" class="rounded-circle" alt="">
+        </div>
+        <div class="col-7 offset-1">
+            <div> <h1> Ahmed Ryan </h1> </div>
+            <div class="d-flex">
+                <div class="pr-5"> <strong>153</strong> posts </div>
+                <div class="pr-5"> <strong>23k</strong> followers </div>
+                <div class="pr-5"> <strong>212</strong> following </div>
             </div>
+            <div class="font-weight-bold pt-3">
+                ahmed_ryan
+            </div>
+            <div>
+                <p class="m-0"> Knowledge is bliss!<br>Change my mind 💁🏼‍♂ </p>
+            </div>
+            <div>
+                <a class="m-0 p-0" href="#">www.ahmedryan.com</a>
+            </div>
+        </div>
+    </div>
+    <div class="row m-5">
+        <div class="col-4">
+            <img src="https://rb.gy/8qpz1z" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://rb.gy/8qpz1z" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://rb.gy/8qpz1z" alt="" class="w-100">
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,10 +21,15 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel bg-white shadow-sm">
+            <div class=" container">
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div class="pl-5 pr-1" style="border-right: 1px solid #333">
+                        <img src="/svg/instagram-logo.svg" style="height: 35px;" alt="">
+                    </div>
+                    <div class="col-6 pl-3 pt-1">
+                        Instagram
+                    </div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
